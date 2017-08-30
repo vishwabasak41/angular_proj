9c4b4a1766dde7fd04a8101591f8bb251321f6f1
vishwabasak41 <vishwabasak95@gmail.com>
2017-08-30 22:06:31 +0530

    got my videos

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DomSanitizer } from '@angular/platform-browser' 
 @Component({
   selector: 'app-videos',
   templateUrl: './videos.component.html',
@@ -10,20 +10,30 @@ name='vishwa';
 videoList = [
         {
          name: "Item 1",
-         slug: "item-1"   
+         slug: "item-1"  ,
+         embed: "rGlEZpOVjGo" 
         },
         {
          name: "Item 2",
-         slug: "item-2"   
+         slug: "item-2",
+         embed:"IhP3J0j9JmY"  
         },
         {
          name: "Item 3",
-         slug: "item-3"   
+         slug: "item-3",
+         embed:"fKopy74weus"  
         }
     ]
-  constructor() { }
+  constructor(private sanitizer:DomSanitizer) { 
+
+  }
 
   ngOnInit() {
+  }
+
+  getMyVideo(item){
+   return this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/"+item.embed )
+
   }
 
 }
